c77699dfc9242739b5474a7097e891dd00fd5219
Khabeeroh <olajidekabirat5@gmail.com>
2026-09-10 12:56:42 +0100

    Adjusting the AI generated image

--- api/AI.js
+++ api/AI.js
@@ -106,7 +106,7 @@ function getRecipeImage(title) {
 
   return match
     ? recipeImages[match]
-    : "/images/recipe-generated.jpg";
+    : "./images/recipe-generated.jpg";
 }
 // GENERATE RECIPE
 export async function getRecipeFromMistral(ingredientsArr) {
